8d25d431c0475024e09d7c715d70aa9ca4f33890
Elham Shahri <elhamshahri2359@gmail.com>
2023-12-06 20:44:01 +0330

    fix sidebar li style

--- src/components/layouts/Sidebar.jsx
+++ src/components/layouts/Sidebar.jsx
@@ -3,8 +3,12 @@ const Sidebar = () => {
     <nav className="flex flex-row md:flex-col  ">
       <label className="text-white text-2xl ms-5 mb-3 mt-2">Post</label>
       <ul className="flex flex-row md:flex-col ">
-        <li className="py-3 px-8 bg-white opacity-20 ms-10 md:ms-0"><span>All Article</span></li>
-        <li className="py-3 px-8 "><span>New Article</span></li>
+        <li className="py-3 px-8 bg-white bg-opacity-20 ms-10 md:ms-0 ">
+          <span className="text-md text-white ">All Article</span>
+        </li>
+        <li className="py-3 px-8   ">
+          <span className="text-md text-white">New Article</span>
+        </li>
       </ul>
     </nav>
   );
