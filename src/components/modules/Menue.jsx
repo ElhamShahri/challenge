54e3eedf3df7ed09a84b28898a907b51cd693772
Elham Shahri <elhamshahri2359@gmail.com>
2023-12-09 23:16:14 +0330

    add action menu for article table

---
+++ src/components/modules/Menue.jsx
@@ -0,0 +1,113 @@
+// import * as React from "react";
+import * as React from "react";
+import { styled, alpha } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import Divider from "@mui/material/Divider";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+
+const StyledMenu = styled((props) => (
+  <Menu
+    elevation={0}
+    anchorOrigin={{
+      vertical: "bottom",
+      horizontal: "right",
+    }}
+    transformOrigin={{
+      vertical: "top",
+      horizontal: "right",
+    }}
+    {...props}
+  />
+))(({ theme }) => ({
+  "& .MuiPaper-root": {
+    borderRadius: 6,
+    marginTop: theme.spacing(1),
+    minWidth: 180,
+    color: "#373a3c",
+    boxShadow:
+      "rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px",
+    "& .MuiMenu-list": {
+      padding: "4px 0",
+    },
+    "& .MuiMenuItem-root": {
+      "& .MuiSvgIcon-root": {
+        fontSize: 18,
+        color: theme.palette.text.secondary,
+        marginRight: theme.spacing(1.5),
+      },
+      "&:active": {
+        backgroundColor: alpha(
+          theme.palette.primary.main,
+          theme.palette.action.selectedOpacity
+        ),
+      },
+    },
+  },
+}));
+
+const CustomizedMenus = ({ handleEdit, handleDelete }) => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleEditClick = () => {
+    handleEdit();
+    handleClose();
+  };
+
+  const handleDeleteClick = () => {
+    handleDelete();
+    handleClose();
+  };
+
+  return (
+    <div className="bg-dark-sky-blue">
+      <Button
+        id="demo-customized-button"
+        aria-controls={open ? "demo-customized-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        variant="contained"
+        disableElevation
+        onClick={handleClick}
+        endIcon={<KeyboardArrowDownIcon />}
+        sx={{
+          backgroundColor: "#5bc0de",
+          "&:hover": {
+            backgroundColor: "#5bc0de", 
+          },
+        }}
+      >
+        ...
+      </Button>
+      <StyledMenu
+        id="demo-customized-menu"
+        MenuListProps={{
+          "aria-labelledby": "demo-customized-button",
+        }}
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleEditClick} disableRipple>
+          Edit
+        </MenuItem>
+        <Divider sx={{ my: 0.5 }} />
+        <MenuItem onClick={handleDeleteClick} disableRipple>
+          Delete
+        </MenuItem>
+      </StyledMenu>
+    </div>
+  );
+};
+
+export default CustomizedMenus;
--- src/components/modules/PostsTable.jsx
+++ src/components/modules/PostsTable.jsx
@@ -1,6 +1,36 @@
+import { useNavigate } from "react-router-dom";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
 import convertHelpers from "../../utils/helpers/convert.helpers";
+import PaginationRounded from "./PaginationRounded";
+import Menue from "./menue";
+import { useState } from "react";
+import { Divider } from "@mui/material";
 
 const PostsTable = ({ articles }) => {
+  const navigate = useNavigate();
+  const [openAlertDialog, setOpenAlertDialog] = useState(false);
+
+  const handleEdit = () => {
+    navigate("/article/create");
+  };
+
+  const handleDelete = () => {
+    setOpenAlertDialog(true);
+  };
+
+  const handleCloseAlertDialog = () => {
+    setOpenAlertDialog(false);
+  };
+
+  const confirmDeleteHandler = () => {
+    // Display an alert dialog on Delete click
+    alert("Are you sure you want to delete?");
+  };
+  confirmDeleteHandler;
 
   return (
     <div className="w-full ">
@@ -21,20 +51,76 @@ const PostsTable = ({ articles }) => {
             <tr key={index}>
               <td className="text-left px-3 py-2">{index}</td>
               <td className="text-left px-3 py-2">{item.title}</td>
-              
-              <td className="text-left px-3 py-2">{item.author && item.author.username?item.author.username:""}</td>
-              <td className="text-left px-3 py-2">{item.tagList.toString()}</td>
+
               <td className="text-left px-3 py-2">
-                {item.slug}
+                {item.author && item.author.username
+                  ? item.author.username
+                  : ""}
               </td>
-              <td className="text-left px-3 py-2">{convertHelpers.dateConvert(item.createdAt)}</td>
+              <td className="text-left px-3 py-2">{item.tagList.toString()}</td>
+              <td className="text-left px-3 py-2">{item.slug}</td>
               <td className="text-left px-3 py-2">
-                <button>...</button>
+                {convertHelpers.dateConvert(item.createdAt)}
+              </td>
+              <td className="text-left px-3 py-2">
+                {/* <button>...</button> */}
+                <Menue handleEdit={handleEdit} handleDelete={handleDelete} />
               </td>
             </tr>
           ))}
         </tbody>
       </table>
+      <PaginationRounded />
+      <Dialog
+        dir="ltr"
+        open={openAlertDialog}
+        onClose={handleCloseAlertDialog}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+        sx={{
+          '& .MuiDialog-paper': {
+            width: '500px', // Adjust the width as per your requirement
+            maxWidth: '90vw', // Maximum width, responsive approach
+          },
+        }}
+      >
+        <DialogTitle
+          id="alert-dialog-title"
+          sx={{
+            fontFamily: "HelveticaNeue",
+          }}
+        >
+          {"Delete Article"}
+        </DialogTitle>
+        <Divider sx={{ my: 0.3 }} />
+        <DialogContent>
+          <DialogContentText
+            id="alert-dialog-description"
+            sx={{
+              fontFamily: "HelveticaNeue",
+            }}
+          >
+            Are you sure to delete Article
+          </DialogContentText>
+        </DialogContent>
+        <Divider sx={{ mt: 3,mb:0.2 }} />
+        <DialogActions>
+          <div className="px-4 flex flex-row gap-4">
+            <button
+              className=" py-1 px-4  text-charcoal-grey rounded border border-1 border-[#dddddd]"
+              onClick={handleCloseAlertDialog}
+            >
+              No
+            </button>
+            <button
+              className=" py-1 px-4  text-white  rounded bg-brick "
+              onClick={confirmDeleteHandler}
+            >
+              Yes
+            </button>
+          </div>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 };
